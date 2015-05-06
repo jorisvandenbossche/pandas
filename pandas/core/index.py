6b77029c57b6aeb2ea7d503ec6f5ea386ab5464f
Jeff Reback <jeff@reback.net>
2015-05-06 08:16:57 -0400

    use new format_data

--- pandas/core/index.py
+++ pandas/core/index.py
@@ -437,6 +437,51 @@ class Index(IndexOpsMixin, PandasObject):
         """
         space1 = "\n%s" % (' ' * (len(self.__class__.__name__) + 2))
         space2 = "\n%s" % (' ' * (len(self.__class__.__name__) + 1))
+        #sep = ',%s' % space1
+        sep = ','
+        max_seq_items = get_option('display.max_seq_items')
+        formatter = self._formatter_func
+        n = len(self)
+        if n == 0:
+            summary = '[]'
+        elif n == 1:
+            first = formatter(self[0])
+            summary = '[%s]' % first
+        elif n == 2:
+            first = formatter(self[0])
+            last = formatter(self[-1])
+            summary = '[%s%s%s]' % (first, space1, last)
+        elif n > max_seq_items:
+            n = min(max_seq_items//2,10)
+
+            head = sep.join([ formatter(x) for x in self[:n] ])
+            tail = sep.join([ formatter(x) for x in self[-n:] ])
+            summary = '[%s%s...%s%s]' % (head, space1, space1, tail)
+        else:
+            values = [ formatter(x) for x in self ]
+            from math import ceil
+            max_len = max([len(x) for x in values]) + 1
+            n_per_row = (75 - len(self.__class__.__name__) - 2) // max_len
+            n_rows = int(ceil(len(values) / float(n_per_row)))
+            # adjust all values to max length
+            values = [x.rjust(max_len) for x in values]
+            summary = '['
+            for i in range(n_rows - 1):
+                summary += sep.join(values[i*n_per_row:(i+1)*n_per_row])
+                summary += sep
+                summary += space1
+            summary += sep.join(values[(n_rows - 1)*n_per_row:n_rows*n_per_row])
+            summary += ']'
+            summary += space1
+
+        return summary
+
+    def _format_data2(self):
+        """
+        Return the formatted data as a unicode string
+        """
+        space1 = "\n%s" % (' ' * (len(self.__class__.__name__) + 2))
+        space2 = "\n%s" % (' ' * (len(self.__class__.__name__) + 1))
         sep = ',%s' % space1
         max_seq_items = get_option('display.max_seq_items')
         formatter = self._formatter_func
@@ -462,7 +507,7 @@ class Index(IndexOpsMixin, PandasObject):
 
         return summary
 
-    def _format_data2(self):
+    def _format_data3(self):
         """
         Return the formatted data as a unicode string
         """
