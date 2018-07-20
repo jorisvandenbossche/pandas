d5aaa9bdfb85e10c620c4c559f9b733439c7f9fe
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-07-20 22:58:15 +0200

    CLN: make core.arrays.timedelta consistent with other array types

--- pandas/core/arrays/__init__.py
+++ pandas/core/arrays/__init__.py
@@ -5,6 +5,6 @@ from .categorical import Categorical  # noqa
 from .datetimes import DatetimeArrayMixin  # noqa
 from .interval import IntervalArray  # noqa
 from .period import PeriodArrayMixin  # noqa
-from .timedeltas import TimedeltaArrayMixin  # noqa
+from .timedelta import TimedeltaArrayMixin  # noqa
 from .integer import (  # noqa
     IntegerArray, to_integer_array)
