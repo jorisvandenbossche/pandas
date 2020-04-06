55db9137e474a9eabe92066edf5441a01cbf7480
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-04-06 10:49:57 +0200

    column-wise

--- pandas/core/ops/__init__.py
+++ pandas/core/ops/__init__.py
@@ -405,9 +405,16 @@ def dispatch_to_series(left, right, func, str_rep=None, axis=None):
     elif isinstance(right, ABCDataFrame):
         assert right._indexed_same(left)
 
-        array_op = get_array_op(func, str_rep=str_rep)
-        bm = operate_blockwise(left, right, array_op)
-        return type(left)(bm)
+        arrays = []
+        for l, r in zip(left._iter_column_arrays(), right._iter_column_arrays()):
+            arrays.append(func(l, r))
+        return type(left)._from_arrays(
+            arrays, left.columns, left.index, verify_integrity=False
+        )
+
+        # array_op = get_array_op(func, str_rep=str_rep)
+        # bm = operate_blockwise(left, right, array_op)
+        # return type(left)(bm)
 
     elif isinstance(right, ABCSeries) and axis == "columns":
         # We only get here if called via _combine_series_frame,
